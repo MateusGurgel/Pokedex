ed102e32623ed31609d80239f030d647d398459b
Mateus Gurgel <mateusgurgel15@gmail.com>
2023-04-27 17:54:08 -0300

    theme defined

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+  colors: {
+    background: '#FF1212',
+    white: '#D9D9D9',
+    blue: '#0DC6F0',
+
+
+
+    greenBar: '04FF4A',
+    blueBar: '4BCEE0',
+    redBar: 'FF1212',
+  },
+})
+
+export default theme
