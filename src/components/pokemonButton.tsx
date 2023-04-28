fca4a3aba51fb9cd9db77879ac61df487357dcc7
Mateus <mateusgurgel15@gmail.com>
2023-04-28 09:18:45 -0300

    pokemon button animation added

--- src/components/pokemonButton.tsx
+++ src/components/pokemonButton.tsx
@@ -1,5 +1,6 @@
 import { MdCatchingPokemon } from "react-icons/md";
 import { Flex } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 interface PokemonButtonProps {
   code: string;
@@ -8,21 +9,23 @@ interface PokemonButtonProps {
 
 export function PokemonButton(props: PokemonButtonProps) {
   return (
-    <Flex
-      width={"30vw"}
-      maxWidth={520}
-      height={"70px"}
-      borderWidth={20}
-      borderRadius={"64px 0px 0px 64px;"}
-      borderColor={"white"}
-      background={"white"}
-      align={"center"}
-      padding={"0"}
-      gap={4}
-    >
-      <MdCatchingPokemon color="red" size={"50px"} />
-      <p>{props.code}</p>
-      <p>{props.name}</p>
-    </Flex>
+    <motion.button whileHover={{ scale: 1.03 }} whileTap={{ scale: 1 }}>
+      <Flex
+        width={"30vw"}
+        maxWidth={520}
+        height={"70px"}
+        borderWidth={20}
+        borderRadius={"64px 0px 0px 64px;"}
+        borderColor={"white"}
+        background={"white"}
+        align={"center"}
+        padding={"0"}
+        gap={4}
+      >
+        <MdCatchingPokemon color="red" size={"50px"} />
+        <p>{props.code}</p>
+        <p>{props.name}</p>
+      </Flex>
+    </motion.button>
   );
 }
